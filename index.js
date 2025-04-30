a60bb0421f4db1539758d3d6a7a7cfc1cfc19bd8
Kaushal-2508 <kaushal.vanshika25@gmail.com>
2025-05-01 01:01:39 +0530

    Update index.js
    adding the button js that make you go to the top

--- index.js
+++ index.js
@@ -27,6 +27,23 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+  // Back to Top Button
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 300) {
+    backToTop.classList.add('active');
+  } else {
+    backToTop.classList.remove('active');
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
 
   // Initialize by showing all images
   const allButton = document.querySelector('.item[data-name="all"]');
